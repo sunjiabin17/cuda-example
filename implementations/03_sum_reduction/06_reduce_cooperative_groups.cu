df22bef017351ac2ccba88976dd06373a91c3e49
sunjiabin <sunjiabin01@outlook.com>
2023-01-16 13:59:23 +0800

    bug fix

--- implementations/03_sum_reduction/06_reduce_cooperative_groups.cu
+++ implementations/03_sum_reduction/06_reduce_cooperative_groups.cu
@@ -19,26 +19,30 @@ using std::generate;
 __device__ int thread_sum(int *input, int n) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     int sum = 0;
-    for (int i = tid; i < n / 4; i += blockDim.x * gridDim.x) {
-        int4 input4 = reinterpret_cast<int4*>(input)[i];
+    // for (int i = tid; i < n / 4; i += blockDim.x * gridDim.x) {
+    //     int4 input4 = reinterpret_cast<int4*>(input)[i];
+    //     sum += input4.x + input4.y + input4.z + input4.w;
+    // }
+    if (tid < n/4) {
+        int4 input4 = reinterpret_cast<int4*>(input)[tid];
         sum += input4.x + input4.y + input4.z + input4.w;
     }
     return sum;
 }
 
 // Reduces a thread group to a single element
-__device__ int reduce_sum_thread_group(thread_group g, int *shared, int val) {
+__device__ int reduce_sum_thread_group(const thread_group& g, int *shared, int val) {
     int lane = g.thread_rank();
+    shared[lane] = val;
+    g.sync();
 
     for (int i = g.size() / 2; i > 0; i >>= 1) {
-        shared[lane] = val;
-        g.sync();
         if (lane < i) {
-            val += shared[lane + i];
+            shared[lane] += shared[lane + i];
         }
         g.sync();
     }
-    return val;
+    return shared[lane];
 }
 
 __global__ void reduce(int *input, int *output, int n) {
@@ -46,39 +50,37 @@ __global__ void reduce(int *input, int *output, int n) {
     extern __shared__ int shared[];
     auto g = this_thread_block();
     int block_sum = reduce_sum_thread_group(g, shared, my_sum);
+    // printf("g.thread_rank()=%d, block_sum=%d\n", g.thread_rank(), block_sum);
     if (g.thread_rank() == 0) {
+        // printf("g.size()=%d, threadIdx=%d, block_sum=%d\n", g.size(), threadIdx.x + blockIdx.x * blockDim.x, block_sum);
         atomicAdd(output, block_sum);
     }
 }
 
 int main() {
-    constexpr int N = 1 << 13;
-    int size = N * sizeof(int);
-    vector<int> h_input(N);
-    vector<int> h_output(N);
+    constexpr int N = 1 << 20;
+    size_t bytes = N * sizeof(int);
+    int *output, *input;
+ 
+    cudaMallocManaged(&output, sizeof(int));
+    cudaMallocManaged(&input, bytes);
+    std::fill(input, input + N, 1);
 
-    generate(h_input.begin(), h_input.end(), []() { return rand() % 100; });
-    int h_sum = accumulate(h_input.begin(), h_input.end(), 0);
+    int h_sum = accumulate(input, input + N, 0);
 
-    int *d_input, *d_output;
-    cudaMalloc(&d_input, size);
-    cudaMalloc(&d_output, size);
-
-    cudaMemcpy(d_input, h_input.data(), size, cudaMemcpyHostToDevice);
     
     constexpr int THREADS = 256;
     constexpr int BLOCKS = (N + THREADS - 1) / THREADS;
 
-    reduce<<<BLOCKS, THREADS, N * sizeof(int)>>>(d_input, d_output, N);
-
-    cudaMemcpy(h_output.data(), d_output, size, cudaMemcpyDeviceToHost);
+    reduce<<<BLOCKS/4, THREADS, THREADS * sizeof(int)>>>(input, output, N);
+    cudaDeviceSynchronize();
 
 
     cout << "Host sum: " << h_sum << endl;
-    cout << "Device sum: " << h_output[0] << endl;
+    cout << "Device sum: " << output[0] << endl;
 
-    cudaFree(d_input);
-    cudaFree(d_output);
+    cudaFree(output);
+    cudaFree(input);
 
     return 0;
 }
